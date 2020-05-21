ce709283a7d5ced02b1b08733288dd5943b3e927
yejiaxin <839994148@qq.com>
2020-05-21 22:10:56 +0800

    放行options

--- src/main/java/com/webmaster/end/config/MyMVCConfiguration.java
+++ src/main/java/com/webmaster/end/config/MyMVCConfiguration.java
@@ -1,6 +1,7 @@
 package com.webmaster.end.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,15 @@ public class MyMVCConfiguration implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         MyInterceptor myInterceptor = new MyInterceptor();
         registry.addInterceptor(myInterceptor).addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") //所有方法
+                .allowedOrigins("*") //允许的域名
+                .allowCredentials(true)
+                .allowedHeaders("*") // 允许请求头
+                .allowedMethods("GET", "POST", "DELETE", "PUT","OPTIONS")//允许方法
+                .maxAge(3600); //表明在3600秒内，不需要再发送预检验请求，可以缓存该结果
     }
 }
